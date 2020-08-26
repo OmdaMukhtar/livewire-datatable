4501a6edcf256b1dc8080dc954a764370ac75179
OmdaMukhtar <abuoop123@gmail.com>
2020-08-26 22:35:41 +0200

    inital commit

---
+++ resources/views/livewire/datatable.blade.php
@@ -0,0 +1,18 @@
+<div class="mt-100">
+    <table class="table" cellspacing="0" width="100%">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th class="th-sm">Task
+            </th>
+            <th class="th-sm">Status
+            </th>
+            <th class="th-sm">Start date
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr><td>11</td></tr>
+        </tbody>
+    </table>
+</div>
